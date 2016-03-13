4e01596c954598fbf0e2b51d0f8b174908e04c22
KaroAntonio <karoantonio@gmail.com>
2016-03-13 15:18:36 -0400

    Sequence Size Validation

--- data.py
+++ data.py
@@ -31,6 +31,9 @@ class Data:
 		print('Loading Data...')
 		self.train = self.load_data()
 		self.test = self.load_data()
+		min_size = self.params['n_steps']*self.params['batch_size']
+		if len(self.train) < min_size:
+			raise Exception('Data Sequence must be of length at least n_steps*batch_size: '+ min_size)
 
 		self.params['n_input'] = self.train.shape[1]
 		self.params['n_batches'] = self.train.shape[0]//self.batch_size
